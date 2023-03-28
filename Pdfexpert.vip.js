cae511628b1d510e7cb504e47c5c9003281e05d9
shwang1114 <36946924+shwang1114@users.noreply.github.com>
2023-03-29 01:34:33 +0800

    Update Pdfexpert.vip.js

--- Pdfexpert.vip.js
+++ Pdfexpert.vip.js
@@ -1,6 +1,7 @@
 var obj = JSON.parse($response.body);
 
 obj["newAccount"]=true;
+obj["responseCreationDate"]="17:12 28\/03\/9999";
 obj["linkedProducts"][0]["inAppStates"][0]["state"]="active";
 obj["linkedProducts"][0]["inAppStates"][0]["expirationDate"]="17:12 04\/04\/9999";
 
