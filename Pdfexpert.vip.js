7891bfadcd06b00676b56c32a505814187f15960
shwang1114 <36946924+shwang1114@users.noreply.github.com>
2023-03-29 01:43:50 +0800

    Update Pdfexpert.vip.js

--- Pdfexpert.vip.js
+++ Pdfexpert.vip.js
@@ -1,7 +1,7 @@
 var obj = JSON.parse($response.body);
-obj["responseCreationDate"] = "17:12 04\/04\/9999";
+obj["responseCreationDate"] = "17:12 04\/04\/2099";
 obj["linkedProducts"][0]["inAppStates"][0]["state"]="active";
-obj["linkedProducts"][0]["inAppStates"][0]["expirationDate"]="17:12 04\/04\/9999";
+obj["linkedProducts"][0]["inAppStates"][0]["expirationDate"]="17:12 04\/04\/2099";
 
 $done({body: JSON.stringify(obj)});
 
