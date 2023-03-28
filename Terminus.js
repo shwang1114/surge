91e266b33e4b295e76a0884866ebad660c4282d6
shwang1114 <36946924+shwang1114@users.noreply.github.com>
2023-03-28 23:55:57 +0800

    Update Terminus.js

--- Terminus.js
+++ Terminus.js
@@ -1,7 +1,7 @@
 let obj=JSON.parse($response.body)
-obj.account["pro_mode"] = true;
-obj.account["plan_type"] = "Premium";
-obj.account["user_type"] = "Premium";
-obj.account["current_period"]["until"] = "2099-10-10T03:27:34";
+obj.bulk_account.account["pro_mode"] = true;
+obj.bulk_account.account["plan_type"] = "Premium";
+obj.bulk_account.account["user_type"] = "Premium";
+obj.bulk_account.account["current_period"]["until"] = "2099-10-10T03:27:34";
 $done({body:JSON.stringify(obj)})
 //bynubyta
