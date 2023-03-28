169a764f057506b08f6cea54aed1f9cb32d7b58e
shwang1114 <36946924+shwang1114@users.noreply.github.com>
2023-03-29 00:12:50 +0800

    Update Terminus.js

--- Terminus.js
+++ Terminus.js
@@ -3,5 +3,6 @@ obj.bulk_account.account["pro_mode"] = true;
 obj.bulk_account.account["plan_type"] = "Premium";
 obj.bulk_account.account["user_type"] = "Premium";
 obj.bulk_account.account["current_period"]["until"] = "2099-10-10T03:27:34";
+obj.bulk_account.account["expired_screen_type"] = "";
 $done({body:JSON.stringify(obj)})
 //bynubyta
