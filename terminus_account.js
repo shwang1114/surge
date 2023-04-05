ac1ae49ad812a93d369042d205e5d196b1a14ae9
shwang1114 <36946924+shwang1114@users.noreply.github.com>
2023-04-05 19:00:44 +0800

    Update terminus_account.js

--- terminus_account.js
+++ terminus_account.js
@@ -4,5 +4,11 @@ obj.account["plan_type"] = "Premium";
 obj.account["user_type"] = "Premium";
 obj.account["current_period"]["until"] = "2099-10-10T03:27:34";
 obj.account["expired_screen_type"] = "";
+obj.trial["trial_type"] = "Team";
+
+obj.trial["valid_until"] = "9999-04-19T10:29:12";
+obj.trial["is_active"] = true;
+
+
 $done({body:JSON.stringify(obj)})
 //bynubyta
